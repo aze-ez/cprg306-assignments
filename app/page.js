e077fdd23ed3dbdccf69047b414b8fa8230e0406
aze-ez <habeebabdulazeez59@gmail.com>
2025-01-29 01:19:27 -0700

    Assignment 2 complete
    Completed assignment 2

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <p>
+        <Link href="/week-2">Goo to Week 2 Assignment/lab</Link>
+      </p>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+ return (
+<div>
+      <h2>Habeeb AbdulAzeez</h2>
+      <p>
+        <Link href="https://github.com/aze-ez/cprg306-assignments">
+          GitHub Repository
+        </Link>
+      </p>
+<p>URL: https://github.com/aze-ez/cprg306-assignments</p>
+    </div>
+  );
+}
