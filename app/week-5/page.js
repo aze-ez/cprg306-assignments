b6059e86eedef7e37d2ff32e1da82c8aa9ba9891
aze-ez <habeebabdulazeez59@gmail.com>
2025-02-25 14:44:23 -0700

    Week 5 Assignment complete
    Week 5 Assignment complete

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Week5() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4"></h1>
+      <NewItem />
+    </div>
+  );
+}
